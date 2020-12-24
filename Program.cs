17b58fc4b455b0c726e10d9313089976f3ae0f0c
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-12-24 16:56:07 -0300

    criado programa

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace URI_1080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor =0;
+            int maiorValor=0;
+            int posição =0;
+
+            for (int i = 1; i <=100; i ++){
+                valor = int.Parse(Console.ReadLine());
+                if (valor > maiorValor){    
+                    maiorValor = valor;
+                    posição = i;
+                }            
+            }
+            Console.WriteLine(maiorValor);
+            Console.WriteLine(posição);
+        }
+    }
+}
